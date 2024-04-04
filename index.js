e8cff491aa972fd0897e0db3baeab58e9aa28159
angularvandan <vandankumar98522@gmail.com>
2024-04-04 12:11:08 +0530

    json-server

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer=require("json-server");
+const server=jsonServer.create();
+const router=jsonServer.router("db.json");
+const middlewere=jsonServer.defaults();
+const port=process.env.PORT || 8080;
+
+server.use(middlewere);
+server.use(router);
+
+server.listen(port);
